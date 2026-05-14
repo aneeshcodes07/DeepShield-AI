fecfa0c76f542bad0556dc6194d56bbb4779b35c
Aneesh VS <aneeshtech07@gmail.com>
2026-05-15 01:48:32 +0530

    Connected frontend to deployed backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,7 +12,7 @@ function App() {
     setResult("");
 
     const response = await axios.post(
-      "http://127.0.0.1:5000/predict"
+      https://deepshield-ai-k0uw.onrender.com/predict
     );
 
     setTimeout(() => {
